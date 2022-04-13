88f5e3cff835d44aea19975ffee83ad1fc38edf8
lovelanai <94110810+lovelanai@users.noreply.github.com>
2022-04-14 00:15:11 +0200

    fixed main

--- src/Main.tsx
+++ src/Main.tsx
@@ -5,7 +5,12 @@ import "./Main.css";
 export default function Main() {
   return (
     <div className="mainContainer">
-      <div className="nameContent"></div>
+      <div className="nameContent">
+        <div>
+          <h1>Love Lanai</h1>
+          <h2>Front End Developer</h2>
+        </div>
+      </div>
       <div className="particlesLayout">
         <ParticlesBg type="cobweb" color="#ffffff" num={30} bg={true} />
       </div>
